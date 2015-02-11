d0ff811fa0cf97191bb301154ef7ddd4a764c30c
Keisuke Ota <arvelt.s@gmail.com>
2015-02-11 19:15:01 +0900

    dichotomous_splitを切り出した

--- main.py
+++ main.py
@@ -21,19 +21,39 @@ class Dungeon:
         target_col = end_col
         for row_index, row in enumerate(self.dungeon[start_row:end_row]):
             for col_index, col in enumerate(row[start_col:end_col]):
-                if (count % 2 == 0):
-                    target_row = (end_row - start_row) / 2
-                    if (row_index == target_row):
-                        self.dungeon[row_index][col_index] = 1
-                else :
-                    target_col = (end_col - start_col) / 2
-                    if (target_col == col_index):
-                        self.dungeon[row_index][col_index] = 1
+                target_range = self.dichotomous_split(count, row_index, start_row, start_col, col_index, end_row, end_col)
+                begin_row = target_range[0]
+                begin_col = target_range[1]
+                target_row = target_range[2]
+                target_col = target_range[3]
+
         count = count - 1
         if (count == 0):
             return
         else:
             self.fill_range(count, begin_row, begin_col, target_row, target_col)
+
+    def dichotomous_split(self, count, row_index, start_row, start_col, col_index, end_row, end_col):
+        begin_row = start_row
+        begin_col = start_col
+        target_row = end_row
+        target_col = end_col
+
+        if (count % 2 == 0):
+            target_row = (end_row - start_row) / 2
+            if (row_index == target_row):
+                self.dungeon[row_index][col_index] = 1
+        else :
+            target_col = (end_col - start_col) / 2
+            if (col_index == target_col):
+                self.dungeon[row_index][col_index] = 1
+
+        return (
+            begin_row,
+            begin_col,
+            target_row,
+            target_col
+        )
 
 
 dungeon = Dungeon()
