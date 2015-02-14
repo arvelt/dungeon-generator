7cf27af627e3fb42d09525eacac8e68ff3c50ae4
Keisuke Ota <arvelt.s@gmail.com>
2015-02-15 03:11:54 +0900

    add main.py

---
+++ main.py
@@ -0,0 +1,224 @@
+# -*- coding: UTF-8 -*-
+import random, math, pprint, pytest
+
+class Rect(object):
+    """ Shape of map base.
+        All unit of member value is squares, not coordinate.
+    """
+
+    def __init__(self, x, y, width, height):
+        """
+        arguments:
+        x -- potision x
+        y -- potision y
+        width -- shape width
+        height -- shape height
+        """
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.ax = x + width -1
+        self.ay = y + height -1
+
+    def __str__(self):
+        return str({
+            'x': self.x,
+            'y': self.y,
+            'width': self.width,
+            'heigth': self.height,
+        })
+
+
+class Tile(object):
+    """ A tile of map in dungeon.
+        All unit of member value is squares, not coordinate.
+    """
+
+    DEFAULT = 'T'
+    WALL = 'W'
+    WAY = 'R'
+    PARTING_LINE = 'A'
+
+
+    def __init__(self, x, y, kind = None):
+        """
+        arguments:
+        x -- potision x
+        y -- potision y
+        kind -- kind of map (Default is 'T')
+        """
+        self.x = x
+        self.y = y
+        if kind is None:
+            self.kind = self.DEFAULT
+        else:
+            self.kind = kind
+
+    def __str__(self):
+        return str(self.kind)
+
+
+class Rooms(object):
+    """ Collection of room.
+    """
+    rooms = ''
+
+    def __init__(self):
+        self.rooms = []
+
+    def add_room(self, room):
+        self.rooms.append(room)
+
+    def __str__(self):
+        return str([str(room) for room in self.rooms])
+
+    def to_map(self, x, y, ax, ay):
+        """ Return squares as a string filled with tiles
+
+        arguments:
+        x -- potision x
+        y -- potision y
+        ax -- potision lower right
+        ay -- potision lower left
+        """
+
+        floor = ''
+        for row in range(x, ax + 1):
+            line = ''
+            for col in range(y, ay + 1):
+                for room in self.rooms:
+                    if room.has_tile(row, col):
+                        tile = room.get_tile(row, col)
+                        line = line + str(tile)
+                        break
+                else:
+                    line = line + str(0)
+            floor = floor + line + '\n'
+        return floor
+
+
+class Room(Rect):
+    """ Room in dungeon.
+        Once instantiated, it is filled with Tile.
+    """
+
+    tiles = ''
+
+    def __init__(self, x, y, width, height, tmp_kind = None):
+        super(Room, self).__init__(x, y, width, height)
+        self.tiles = []
+        self.fill_tiles(tmp_kind)
+
+    def fill_tiles(self, tmp_kind):
+        for row in range(self.x, self.ax + 1):
+            for col in range(self.y, self.ay + 1):
+                self.tiles.append(Tile(row , col, tmp_kind))
+
+    def get_tile(self, x, y):
+        for tile in self.tiles:
+            if tile.x == x and tile.y == y:
+                return tile
+        else:
+            return None
+
+    def has_tile(self, x, y):
+        if (self.x <= x and x <= self.x + self.width - 1) and \
+            (self.y <= y and y <= self.y + self.height - 1):
+            return True
+        else:
+            return False
+
+
+class OuterFrame(Rect):
+    """ The outer frame in dungeon.
+        This class has origin potisioin. Usually (1, 1).
+        This class provied pop_room method.
+    """
+    rooms = ''
+
+    def __init__(self, width, height, config=None):
+        """
+        arguments:
+        width -- Outer frame width. (Mean the size of dungeon width)
+        height -- Outer frame height. (Mean the size of dungeon height)
+        """
+        super(OuterFrame, self).__init__(1, 1, width, height)
+        self.rooms = Rooms()
+        if config is not None:
+            self.config = config
+        else:
+            self.config = self.default_config
+        self.pop_rooms()
+
+    @property
+    def default_config(self):
+        return {
+            'room_number' : 1
+        }
+
+    def pop_rooms(self):
+        """ Pop rooms in dungeons.
+            See RoomGenerator details.
+        """
+        room_generator = RoomGenerator(self.width, self.height, self.config)
+        sizes = room_generator.get_room_sizes()
+        for index, size in enumerate(sizes):
+            print size
+            room = Room(size.get('x'), size.get('y'), size.get('width'), size.get('height'))
+            self.rooms.add_room(room)
+
+    def __str__(self):
+        return str(self.rooms)
+
+    def to_map(self):
+        return self.rooms.to_map(self.x, self.y, self.ax, self.ay)
+
+
+class RoomGenerator(Rect):
+    """ Generator of room in dungeon.
+    """
+    def __init__(self, width, height, config):
+        super(RoomGenerator, self).__init__(1, 1, width, height)
+        self.room_number = config.get('room_number', 4)
+
+    def get_room_sizes(self):
+        """ Return Size of the room to make future.
+            WIP.
+        """
+        room_sizes = []
+        for num in range(self.room_number):
+            width = random.randint(self.width / 4, self.width / 4 + 10)
+            height = random.randint(self.height / 4, self.height / 4 + 10)
+            x = random.randint(self.x, self.x + self.width - width -3)
+            y = random.randint(self.y, self.y + self.height - height -3)
+            room_sizes.append({
+                'x': x,
+                'y': y,
+                'width': width,
+                'height': height
+            })
+        return room_sizes
+
+
+class Dungeon:
+    row_size = 64
+    col_size = 64
+
+    def __init__(self, config=None):
+        self.dungeon = OuterFrame(self.row_size, self.col_size, config)
+
+
+    def __str__(self):
+        return str(self.dungeon)
+
+    def to_map(self):
+        return self.dungeon.to_map()
+
+
+if __name__ == '__main__':
+    config = {
+        'room_number' : 6
+    }
+    dungeon = Dungeon()
+    print dungeon.to_map()
