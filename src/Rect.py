2eb3b1c9a94599ecbfaca39c168b1cf0db3577f8
Keisuke Ota <arvelt.s@gmail.com>
2015-03-06 02:38:36 +0900

    adjust documents Rect

--- src/Rect.py
+++ src/Rect.py
@@ -2,25 +2,34 @@
 
 class Rect(object):
     """ Shape of map base.
+
         All unit of member value is squares, not coordinate.
     """
 
     def __init__(self, x, y, width, height):
-        """
-        arguments:
-        x -- potision x
-        y -- potision y
-        width -- shape width
-        height -- shape height
-        """
         self.x = x
+        """X squares in the lower right."""
+
         self.y = y
+        """Y squares in the lower right."""
+
         self.width = width
+        """Size of width"""
+
         self.height = height
+        """Size of height"""
+
         self.ax = x + width -1
+        """X squares in the lower right. Automatic calculation from the argument"""
+
         self.ay = y + height -1
+        """Y squares in the lower right. Automatic calculation from the argument"""
+
         self.mx = (self.x + self.ax) / 2
+        """X squares midpoint. Automatic calculation from the argument"""
+
         self.my = (self.y + self.ay) / 2
+        """Y squares midpoint. Automatic calculation from the argument"""
 
     def __str__(self):
         return str({
