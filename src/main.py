990212f7bedf8a283aaf31af83dc67f16105c9d8
Keisuke Ota <arvelt.s@gmail.com>
2015-03-07 14:05:37 +0900

    tweakt docs

--- src/main.py
+++ src/main.py
@@ -2,16 +2,25 @@
 from Frame import Frame
 
 class Dungeon:
+    """ Generator two-dimensional map for roguelike.
+
+        Config:
+
+        - row_size: Vertical size of the map.
+        - col_size: Horizontal size of the map.
+        - room_number: Number of rooms to pop in the map.
+    """
     def __init__(self, config={}):
         self.row_size = config.get('col_size', 20)
         self.col_size = config.get('row_size', 20)
         self.dungeon = Frame(self.col_size, self.row_size, config)
 
-
-    def __str__(self):
-        return str(self.dungeon)
-
     def to_string(self):
+        """Get a human-readable dungeon map.
+
+        :rtype: string
+        :return: Fixed-length string with a newline.
+        """
         return self.dungeon.to_string()
 
 
