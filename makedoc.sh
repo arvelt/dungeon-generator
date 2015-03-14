19b0b660365175f5bf5dd2b9f692dcf1e9dff62d
Keisuke Ota <arvelt.s@gmail.com>
2015-03-14 23:49:36 +0900

    update document

--- makedoc.sh
+++ makedoc.sh
@@ -1,4 +1,9 @@
+# 自分の現在のリポジトリのgh-pagesをdocsの配下にsubmoduleとして登録する
+# git submodule add -b gh-pages `git remote -v|grep fetch|awk '{print$2}'` docs
+
 #first time
 sphinx-apidoc -F -o ./documents_source ./src
 #sphinx-apidoc -f -o ./documents_source ./src
 sphinx-build -a ./documents_source ./publish
+
+#cp -rf publish/* docs/
